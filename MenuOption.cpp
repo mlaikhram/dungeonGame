63655d0a1d5744a9c2360ef909bc8dea4310d315
Matthew Laikhram <mlaikhram@hotmail.com>
2017-06-29 03:57:31 -0400

    cleaned comments and changed menuoption to use string instead of char array

--- MenuOption.cpp
+++ MenuOption.cpp
@@ -2,34 +2,17 @@
 
 #include <string>
 
-MenuOption::MenuOption(std::string p, Vector3 position, const char *spriteSheetName, int numx, int numy, float minSize, float maxSize, float spacing) : 
-	position(position), numx(numx), numy(numy), tileSize(minSize), minSize(minSize), maxSize(maxSize), spacing(spacing) {
-	// Convert string 'p' into char array phrase
-	phrase = new char[p.length()];
-	for (int i = 0; i < p.length(); ++i) {
-		phrase[i] = p.at(i);
-	}
-
-	// Store length of phrase
-	lengthOfPhrase = p.length();
-
+MenuOption::MenuOption(std::string phrase, Vector3 position, const char *spriteSheetName, int numx, int numy, float minSize, float maxSize, float spacing) : 
+	phrase(phrase), position(position), numx(numx), numy(numy), tileSize(minSize), minSize(minSize), maxSize(maxSize), spacing(spacing) {
 	// Load the spritesheet
 	spriteSheet = LoadTexture(spriteSheetName);
 }
 
-//float MenuOption::getWidth() {
-//	return width;
-//}
-//
-//float MenuOption::getHeight() {
-//	return height;
-//}
-
 bool MenuOption::pointCollision(float mousex, float mousey) {
-	float width = lengthOfPhrase * (tileSize - spacing * tileSize) - tileSize / 2;
+	float width = phrase.length() * (tileSize - spacing * tileSize) - tileSize / 2;
 
 	float left = position.x - tileSize / 2.0f - width / 2;
-	float right = left + lengthOfPhrase * tileSize - (spacing * tileSize * lengthOfPhrase); //subtract for spacing
+	float right = left + phrase.length() * tileSize - (spacing * tileSize * phrase.length()); //subtract for spacing
 	float top = position.y + tileSize / 2.0f;
 	float bottom = top - tileSize;
 	return (mousex > left && mousex < right && mousey > bottom && mousey < top);
@@ -38,9 +21,9 @@ bool MenuOption::pointCollision(float mousex, float mousey) {
 void MenuOption::draw(ShaderProgram *program, Matrix &projectionMatrix, Matrix &modelMatrix, Matrix &viewMatrix) {
 
 	//float width = (position.x + (lengthOfPhrase - 1) * tileSize * spacing + tileSize / 2) - (position.x - tileSize / 2);
-	float width = lengthOfPhrase * (tileSize - spacing * tileSize) - tileSize / 2;
+	float width = phrase.length() * (tileSize - spacing * tileSize) - tileSize / 2;
 
-	for (int i = 0; i < lengthOfPhrase; ++i) {
+	for (int i = 0; i < phrase.length(); ++i) {
 		modelMatrix.identity();
 		modelMatrix.Translate((position.x + i*tileSize - (spacing * tileSize * i)) - width / 2, position.y, 0.0f); //subtract for spacing
 		program->setModelMatrix(modelMatrix);
--- MenuOption.h
+++ MenuOption.h
@@ -8,22 +8,17 @@
 
 class MenuOption {
 public:
-	//MenuOption(); // Constructors
-	MenuOption(std::string p, Vector3 position, const char *spriteSheetName, int numx, int numy, float minSize, float maxSize, float spacing = 0.4f);
-	//float getWidth();
-	//float getHeight();
+	MenuOption(std::string phrase, Vector3 position, const char *spriteSheetName, int numx, int numy, float minSize, float maxSize, float spacing = 0.4f);
 	bool pointCollision(float mousex, float mousey);
 	void draw(ShaderProgram *program, Matrix &projectionMatrix, Matrix &modelMatrix, Matrix &viewMatrix);
 	void update(ShaderProgram *program, float mousex, float mouesy);
 
 private:
 	Vector3 position;
-	//float width, height;
 	float tileSize;
 	float minSize;
 	float maxSize;
-	char* phrase;
-	int lengthOfPhrase;
+	std::string phrase;
 	GLuint spriteSheet;
 	int numx;
 	int numy;
